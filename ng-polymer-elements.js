a53d83dcf31b129b74d0849330aa1ff5d5d55cac
Gabriel Axel <guznik@gmail.com>
2014-12-09 15:56:51 +0200

    Changed directive registration and custom mapping to not rely on global variable.

--- ng-polymer-elements.js
+++ ng-polymer-elements.js
@@ -61,116 +61,118 @@
         }
     };
     
-    // The following allows adding and modifying mappings by the application
-    
-    var extendedMappings = window.NG_POLYMER_ELEMENTS_EXTENDED_MAPPINGS;
-    
-    if(extendedMappings) {
-    	angular.extend(allMappings, extendedMappings);
-    }
-
     var module = angular.module('ng-polymer-elements', [])
-    
-    // A directive is created for each web component according to the mappings
-    
-    Object.keys(allMappings).forEach(function(tag) {
-        var mappings = allMappings[tag];
+    	.config(['$compileProvider', '$injector', function($compileProvider, $injector) {
 
-        module.directive(tag, ['$parse', '$window', function($parse, $window) {
-
-            var scopeDefinition = {};
-
-            var keys = Object.keys(mappings);
-
-            keys.forEach(function(attr) {
-
-                var conf = mappings[attr];
-
-                if(conf.primitive || conf.object || conf.array) {
-                    scopeDefinition[attr] = '=';
-                } else if(!conf.event) {
-                    throw 'Invalid mapping for ' + attr + 
-                    ' - must contain primitive | object | array | event';
-                }
-            });
-
-            return {
-                restrict: 'E',
-                scope: scopeDefinition,
-
-                link: function (scope, element, attrs) {
-
-                    var el = element[0];
-
-                    keys.forEach(function(attr) {
-
-                    	// Don't create bindings for non-existent attributes
-                    	if(!attrs[attr]) {
-                    		return;
-                    	}
-                    	
-                        var conf = mappings[attr];
-
-                        if(conf.event) {
-                        	
-                        	var fn = $parse(attrs[attr]);
-
-                            el.addEventListener(conf.event, function (e) {
-                            	scope.$apply(function() {
-                            		fn(scope.$parent, {$event: e});
-                            	});
-                                
-                            });
-
-                        } else {
-
-                            var propertyName = 
-                            	conf.primitive || conf.object || conf.array;
-
-                            if(conf.object) {
-                                el[propertyName] = {};
-                            } else if(conf.array) {
-                                el[propertyName] = [];
-                            }
-
-                            // Copy the scope property value to the web 
-                            // component's value
-                            
-                            var handler = function(value) {
-                                if(conf.primitive) {
-                            		el[propertyName] = value;
-                                } else {
-                                    angular.copy(value, el[propertyName]);
-                                }
-                            };
-
-                            scope.$watch(attr, handler, true);
-
-                            handler(scope[attr]);
-                            
-                            // Copy the web component's value to the scope 
-                            // property value
-                            
-                            var observer = new PathObserver(el, propertyName);
-
-                            observer.open(function (value) {
-                                scope.$apply(function () {
-                                    if(conf.primitive) {
-                                        scope[attr] = value;
-                                    } else {
-                                        angular.copy(value, scope[attr]);
-                                    }
-                                });
-                            });
-
-                        }
-
-                    });
-                }
-
-            };
-        }]);
-    });
+    		// Extension point for overriding mappings
+    		
+    		if($injector.has('$ngPolymerMappings')) {
+    			var extendedMappings = $injector.get('$ngPolymerMappings');
+    			angular.extend(allMappings, extendedMappings);
+    		}
+    		
+		    // A directive is created for each web component according to the mappings
+		    
+		    Object.keys(allMappings).forEach(function(tag) {
+		        var mappings = allMappings[tag];
+		
+		        $compileProvider.directive(tag, ['$parse', '$window', function($parse, $window) {
+		
+		            var scopeDefinition = {};
+		
+		            var keys = Object.keys(mappings);
+		
+		            keys.forEach(function(attr) {
+		
+		                var conf = mappings[attr];
+		
+		                if(conf.primitive || conf.object || conf.array) {
+		                    scopeDefinition[attr] = '=';
+		                } else if(!conf.event) {
+		                    throw 'Invalid mapping for ' + attr + 
+		                    ' - must contain primitive | object | array | event';
+		                }
+		            });
+		
+		            return {
+		                restrict: 'E',
+		                scope: scopeDefinition,
+		
+		                link: function (scope, element, attrs) {
+		
+		                    var el = element[0];
+		
+		                    keys.forEach(function(attr) {
+		
+		                    	// Don't create bindings for non-existent attributes
+		                    	if(!attrs[attr]) {
+		                    		return;
+		                    	}
+		                    	
+		                        var conf = mappings[attr];
+		
+		                        if(conf.event) {
+		                        	
+		                        	var fn = $parse(attrs[attr]);
+		
+		                            el.addEventListener(conf.event, function (e) {
+		                            	scope.$apply(function() {
+		                            		fn(scope.$parent, {$event: e});
+		                            	});
+		                                
+		                            });
+		
+		                        } else {
+		
+		                            var propertyName = 
+		                            	conf.primitive || conf.object || conf.array;
+		
+		                            if(conf.object) {
+		                                el[propertyName] = {};
+		                            } else if(conf.array) {
+		                                el[propertyName] = [];
+		                            }
+		
+		                            // Copy the scope property value to the web 
+		                            // component's value
+		                            
+		                            var handler = function(value) {
+		                                if(conf.primitive) {
+		                            		el[propertyName] = value;
+		                                } else {
+		                                    angular.copy(value, el[propertyName]);
+		                                }
+		                            };
+		
+		                            scope.$watch(attr, handler, true);
+		
+		                            handler(scope[attr]);
+		                            
+		                            // Copy the web component's value to the scope 
+		                            // property value
+		                            
+		                            var observer = new PathObserver(el, propertyName);
+		
+		                            observer.open(function (value) {
+		                                scope.$apply(function () {
+		                                    if(conf.primitive) {
+		                                        scope[attr] = value;
+		                                    } else {
+		                                        angular.copy(value, scope[attr]);
+		                                    }
+		                                });
+		                            });
+		
+		                        }
+		
+		                    });
+		                }
+		
+		            };
+		        }]);
+		    });
+		    
+    	}]);
 	
 	module.service('$polymerOpen', function($http, $templateCache, $compile, $rootScope, $window) {
 		
